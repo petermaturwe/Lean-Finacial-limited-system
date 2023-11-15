fd47e9b024bb295a8bfb3256a6e2c6491930ed4e
maturwe <petermaturwe@gmail.com>
2023-11-15 17:25:19 +0300

    added tailwind css and ran it well

--- lean/financial/urls.py
+++ lean/financial/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 from django.conf import settings
-from .views import login_redirect
+
 from django.conf.urls.static import static 
 
 urlpatterns = [
