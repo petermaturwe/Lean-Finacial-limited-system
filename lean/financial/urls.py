6f176c5e05ba55ae5873002ab2e298fbb662787a
maturwe <petermaturwe@gmail.com>
2023-11-15 15:08:51 +0300

    added the project files

---
+++ lean/financial/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from .views import login_redirect
+from django.conf.urls.static import static 
+
+urlpatterns = [
+    path('', views.index, name='index'),
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
